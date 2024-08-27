540b9249a9e45a453515ea1930e6bb3ef9222e1d
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-08-27 13:49:04 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+
+    }
+
+    private static double area(double x, double y) {
+
+        return x;
+    }
+
+    private static double area(double radius) {
+
+        return radius;
+    }
+
+}
